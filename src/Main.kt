80ac0ebaaa7906fa25d0c751b8fc1e4e70cf615a
hussainjg <hussainjg13@gmail.com>
2025-02-18 19:26:56 +0300

    Hussain Jamal

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,34 @@
 fun main() {
-    println("Hello World!")
+    var hussain = UserRole.ADMIN //1
+    var hassan = UserRole.EDITOR //2
+    var bader = UserRole.VIEWER //3
+
+    var phone = 555554652
+
+
+    println("-----------")
+    if(checkAccess(bader, 1)){
+
+        println("calling $phone ..")
+    }else{
+        println("You dont have permission to call $phone!")
+    }
+    println("-----------")
+
+    println("Printing all roles and their access level..")
+    for (role in UserRole.entries) {
+        println("${role.name}: Access Level ${role.accessLevel}")
+    }
 }
+    enum class UserRole(val accessLevel: Int){
+        ADMIN(1),EDITOR(2), VIEWER(3)
+    }
+
+    fun checkAccess(UserRole: UserRole, level: Int): Boolean {
+        if (level == UserRole.accessLevel) {
+            return true
+        }else{
+            return false
+        }
+    }
+
